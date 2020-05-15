dd044955c0abf43bf1df1de5106451e1ba3ae0b8
ckddlr0610 <aackddlr@gmail.com>
2020-05-15 15:39:18 +0900

    xml, BottomNavigationview 수정

--- app/src/main/java/com/example/gunmunity/community/CommunityMainActivity.java
+++ app/src/main/java/com/example/gunmunity/community/CommunityMainActivity.java
@@ -1,25 +1,50 @@
 package com.example.gunmunity.community;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.gunmunity.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CommunityMainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationBar;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community_main);
         initView();
-
     }
 
     private void initView() {
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.main_container, new CommunityMainFragment()).commit();
+
+        bottomNavigationBar = findViewById(R.id.main_bottom_tab);
+        bottomNavigationBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_community:
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.main_container, new CommunityMainFragment()).commit();
+                        break;
+                    case R.id.nav_calculator:
+//                        getSupportFragmentManager().beginTransaction()
+//                                .replace(R.id.main_container, new CommunityMainFragment()).commit();
+                        break;
+                    case R.id.nav_asset:
+//                        getSupportFragmentManager().beginTransaction()
+//                                .replace(R.id.main_container, new CommunityMainFragment()).commit();
+                        break;
+                }
+                return true;
+            }
+        });
 
     }
 
