365a922eb2a0a1f16e5ad5deca9b06069424c8ef
ckddlr0610 <aackddlr@gmail.com>
2020-05-26 23:45:05 +0900

    Splash, Login, Signup 화면 추가

--- app/src/main/java/com/example/gunmunity/model/CommunityList.java
+++ app/src/main/java/com/example/gunmunity/model/CommunityList.java
@@ -5,14 +5,14 @@ public class CommunityList {
     private String content;
     private String time;
     private String nickname;
-    private int comment;
+    private String comment;
 
     public CommunityList() {
         title = "포상휴가 어떻게 받아요?";
         content = "코로나 때문에 휴가 못나가고 있는데 포상휴가 받고 싶다 포상휴가 받는 방법 좀";
         time = "2020.5.12 16:00";
         nickname = "무적해병";
-        comment = 2;
+        comment = "2";
     }
 
     public String getTitle() {
@@ -47,11 +47,11 @@ public class CommunityList {
         return nickname;
     }
 
-    public int getComment() {
+    public String getComment() {
         return comment;
     }
 
-    public void setComment(int comment) {
+    public void setComment(String comment) {
         this.comment = comment;
     }
 }
